c6f8965e98fecb4454278767a7b680bb0f6e7d76
ArielLabs <arielassraf@gmail.com>
2023-05-25 00:52:43 +0300

    create IntroductionPage and RoomPage

---
+++ client/src/pages/IntroductionPage/IntroductionPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./IntroductionPage.module.css";
+
+const IntroductionPage = () => {
+    return(
+        <div></div>
+    );
+}
+
+export default IntroductionPage;
