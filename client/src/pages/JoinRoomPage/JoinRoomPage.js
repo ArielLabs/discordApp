f1c0f32d1959e521b12588ec471855222ea696dd
ArielLabs <arielassraf@gmail.com>
2023-05-28 02:30:25 +0300

    install mui in client and add form in JoinRoomPage

--- client/src/pages/JoinRoomPage/JoinRoomPage.js
+++ client/src/pages/JoinRoomPage/JoinRoomPage.js
@@ -1,25 +1,74 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { roomActions } from "../../store/room";
 import { useLocation } from "react-router-dom";
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import styles from "./JoinRoomPage.module.css";
 
 const JoinRoomPage = () => {
-    const dispatch = useDispatch();
-    const search = useLocation().search;
+  const [roomId, setRoomId] = useState("");
+  const [name, setName] = useState("");
+  const [optionAudio, setOptionAudio] = useState(false);
+  const isRoomHost = useSelector((state) => state.room.isRoomHost);
+  const dispatch = useDispatch();
+  const search = useLocation().search;
 
-    useEffect(() => {
-        const isRoomHost = new URLSearchParams(search).get('host');
-        if(isRoomHost){
-            dispatch(roomActions.setIsRoomHost(true));
-        }
-    }, []);
+  useEffect(() => {
+    const isRoomHostUrl = new URLSearchParams(search).get("host");
+    if (isRoomHostUrl) {
+      dispatch(roomActions.setIsRoomHost(true));
+    } else {
+      dispatch(roomActions.setIsRoomHost(false));
+    }
+  }, []);
 
-    return(
-        <div className={styles.joinRoom}>
-            <div className={styles.joinRoomPanel}></div>
+  const changeRoomIdHandler = (event) => {
+    setRoomId(event.target.value);
+  };
+
+  const changeNameHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const changeOptionAudioHandler = (event) => {
+    setOptionAudio(event.target.checked);
+  }
+
+  const titleText = isRoomHost ? "Host meeting" : "Join meeting";
+
+  return (
+    <div className={styles.joinRoom}>
+      <div className={styles.joinRoomPanel}>
+        <h2>{titleText}</h2>
+        <div className={styles.form}>
+          {!isRoomHost && (
+            <input
+              type="text"
+              className={styles.joinInput}
+              onChange={changeRoomIdHandler}
+              placeholder="Enter meeting ID"
+            />
+          )}
+          <input
+            type="text"
+            className={styles.joinInput}
+            onChange={changeNameHandler}
+            placeholder="Enter your name"
+          />
         </div>
-    );
-}
+        <FormControlLabel
+          label="Only audio"
+          control={
+            <Checkbox
+                checked={optionAudio}
+                onChange={changeOptionAudioHandler} 
+            />
+          }
+        />
+      </div>
+    </div>
+  );
+};
 
 export default JoinRoomPage;
