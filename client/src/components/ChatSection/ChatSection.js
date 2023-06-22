dbdfda255b08604c6c8c08ac662c7a6449ffec3e
ArielLabs <arielassraf@gmail.com>
2023-06-22 20:27:46 +0300

    add wallpaper image to ChatSection

--- client/src/components/ChatSection/ChatSection.js
+++ client/src/components/ChatSection/ChatSection.js
@@ -1,10 +1,10 @@
 import React from "react";
+import wallpapaer from "../../assets/images/wallpaper.jpg";
 import styles from "./ChatSection.module.css";
 
 const ChatSection = () => {
     return(
-        <div className={styles.chatContainer}>
-            chat
+        <div style={{ backgroundImage: `url(${wallpapaer})`, width: "25%" }}>
         </div>
     );
 }
