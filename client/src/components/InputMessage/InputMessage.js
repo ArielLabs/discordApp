8edc41a86ae069e554727d5b5a26ae9d8e1d273a
ArielLabs <arielassraf@gmail.com>
2023-06-25 02:17:33 +0300

    style InputMessage component

--- client/src/components/InputMessage/InputMessage.js
+++ client/src/components/InputMessage/InputMessage.js
@@ -1,10 +1,24 @@
 import React from "react";
+import IconButton from "@mui/material/IconButton";
+import SendIcon from "@mui/icons-material/Send";
 import styles from "./InputMessage.module.css";
 
 const InputMessage = () => {
-    return(
-        <div></div>
-    );
-}
+  const sendMessageHandler = () => {};
+  return (
+    <div className={styles.inputWrapper}>
+      <input
+        className={styles.inputMsg}
+        onKeyDown={sendMessageHandler}
+        placeholder="Send a message..."
+      />
+      <div className={styles.icon}>
+        <IconButton>
+          <SendIcon sx={{ color: "#80e3c1" }} />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
 export default InputMessage;
